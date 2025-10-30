a025c1c9e72f927ae537dab6cc54c7c0c5595f43
Mesgun <Mesgun@MESGUN->
2025-10-30 15:02:43 +0100

    Add project files.

---
+++ BilSimulator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BilSimulator
+{
+    internal class Program
+    {
+        // Mina egna värden till ThingSpeak
+        private const string ChannelId = "3134394";
+        private const string WriteApiKey = "4OYTWST0953SEX3F";
+
+        static async Task Main(string[] args)
+        {
+            // Startposition för bilen (ungefär Stockholm)
+            double latitude = 59.3293;
+            double longitude = 18.0686;
+
+            Console.WriteLine("BilSimulator startar... (Tryck Ctrl + C för att avsluta)\n");
+
+            var slump = new Random();
+            int rpm = slump.Next(800, 1200);
+            int hastighet = 0;
+            int bransle = 100;
+            double temp = 85;
+        }
+    }
+
+} 
+              
+
+
+
+
+          
